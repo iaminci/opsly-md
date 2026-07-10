e92946aaab54b0daba462e97a6ea94870caa363d
Akash Patel <akash@opsly.dev>
2026-07-10 11:34:43 +0530

    feat: google analytics added (#49)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Fira_Code } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
@@ -69,6 +70,18 @@ export default function RootLayout({
       className={firaCode.variable}
     >
       <body className="antialiased">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NVQTEF2STY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NVQTEF2STY');
+          `}
+        </Script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
